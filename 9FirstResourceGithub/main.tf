aadfba964e92945777c23fb5105d947ff4b49aa3
Nitish Kumar <nitishkumar@outlook.com>
2022-05-17 16:39:34 +0530

    9

--- 9FirstResourceGithub/main.tf
+++ 9FirstResourceGithub/main.tf
@@ -1,11 +1,11 @@
-provider "github" {
-  token = "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"
-}
-
 resource "github_repository" "gitrepo" {
-  name= "terraformrepo"
+  name        = "terraformrepo"
   description = "Github repo created through Terraform"
 
   visibility = "public"
-  auto_init = true
+  auto_init  = true
+}
+
+output "gitrepo" {
+  value = github_repository.gitrepo.html_url
 }
---
+++ 9FirstResourceGithub/variable.tf
@@ -0,0 +1,15 @@
+variable "token" {
+  #   default = "XYZ"
+}
+
+variable "user" {
+  default = "Gaurav"
+}
+
+variable "age" {
+  default = 23
+}
+
+variable "city" {
+  default = "Jaipur"
+}
